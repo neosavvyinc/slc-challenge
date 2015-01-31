1ac1a22933de85b636d9a0d472b28f2514ed1604
Trevor Ewen <tewen@neosavvy.com>
2015-01-31 17:23:01 -0500

    All the fun stuff from the initial generation of the firebase app.

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name slcChallengeApp
+ * @description
+ * # slcChallengeApp
+ *
+ * Main module of the application.
+ */
+angular.module('slcChallengeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.utils',
+    'simpleLogin'
+  ]);
