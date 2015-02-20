3b5bf8cec76062de73145bbbb4a13b8d81bc71ca
Trevor Ewen <tewen@neosavvy.com>
2015-02-20 13:46:56 -0500

    Added Sam's requested functional changes.

--- app/scripts/controllers/beer-check-in.js
+++ app/scripts/controllers/beer-check-in.js
@@ -14,20 +14,13 @@ angular.module('slcChallengeApp')
     readOnly.checkIn($routeParams.index).$bindTo($scope, 'checkIn');
 
     var hasRequiredFields = function (checkIn) {
-      return checkIn && checkIn.bar && checkIn.bartender;
+      return checkIn && checkIn.rating;
     };
-
-    //Watchers
-    var de = $scope.$watch('beer', function (val) {
-      if (val && !val.image) {
-        this.localState.checkInFields = true;
-        de();
-      }
-    }.bind(this));
 
     //Controller methods
     this.localState = {
-      checkInFields: false
+      rating: 'unrated',
+      checkInFields: true
     };
     this.checkInExists = function () {
       return $scope.beer && hasRequiredFields($scope.checkIn);
@@ -36,21 +29,12 @@ angular.module('slcChallengeApp')
       e.preventDefault();
       this.localState.checkInFields = !this.localState.checkInFields;
     };
-    this.getReferenceLabel = function (beer) {
-      if (beer) {
-        if (beer.image && beer.description) {
-          return '*Image & Description:';
-        } else if (beer.image) {
-          return '*Image:';
-        } else if (beer.description) {
-          return '*Description:';
-        }
-      }
-      return '';
+    this.getSearchTerm = function () {
+        return $location.search().searchTerm ? '?searchTerm=' + $location.search().searchTerm : '';
     };
     this.submit = function (e) {
       e.preventDefault();
-      updateOnly.checkIn($scope.beer, _.pick(this.localState, ['bar', 'bartender', 'type'])).then(function () {
+      updateOnly.checkIn($scope.beer, _.pick(this.localState, ['bar', 'bartender', 'rating'])).then(function () {
         growl.success($scope.beer.name + ' checked in!');
         $location.path('/');
       }).catch(globalErrorHandler);
