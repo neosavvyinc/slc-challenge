486ed0ae9781ae44415f8132825b6103ff77a663
Trevor Ewen <tewen@neosavvy.com>
2015-04-10 09:04:56 -0400

    Slc challenge changes.

---
+++ app/scripts/directives/pointsRemaining.js
@@ -0,0 +1,8 @@
+angular.module('slcChallengeApp').directive('pointsRemaining', function () {
+  return {
+    template: '<div>3</div>',
+    link: function (scope, element, attrs) {
+
+    }
+  };
+});
